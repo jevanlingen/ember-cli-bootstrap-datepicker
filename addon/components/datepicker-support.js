90336af714095f4ef9ff611744f625d21dbef603
Jacob van Lingen <jacobvanlingen@hotmail.com>
2017-08-18 11:09:51 +0200

    Update datepicker-support.js

--- addon/components/datepicker-support.js
+++ addon/components/datepicker-support.js
@@ -104,7 +104,7 @@ export default Ember.Mixin.create({
 
   _addObservers: Ember.on('didInsertElement', function() {
     this.addObserver('language', function() {
-      this.$().datepicker('remove');
+      this.$().datepicker('destroy');
       this.setupBootstrapDatepicker();
     });
 
